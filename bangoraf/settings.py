cd19e1a8d72d501dafc298f9dd91363e477efe4e
kimjinsoo <1004kjs_@naver.com>
2021-01-11 21:51:52 +0900

    Add : 수정완료

--- bangoraf/settings.py
+++ bangoraf/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import my_settings
+from my_settings import SECRET, DATABASE
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = my_settings.SECRET
+SECRET_KEY = SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'bangoraf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = my_settings.DATABASES
+DATABASES = DATABASE
 
 
 # Password validation
--- my_settings.py
+++ my_settings.py
@@ -1,4 +1,4 @@
-DATABASES = {
+DATABASE = {
     
 }
 
