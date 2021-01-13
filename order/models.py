4b5958cc8fea178b4465259bf0cdfbf560e3fea7
kimjinsoo <1004kjs_@naver.com>
2021-01-13 12:48:52 +0900

    Add : 멘토님 리뷰 수정완료

--- order/models.py
+++ order/models.py
@@ -3,38 +3,60 @@ from user.models    import Users
 from product.models import Products
 
 class Payments(models.Model):
-    user       = models.ForeignKey(Users, on_delete=models.CASCADE)
-    card       = models.CharField(max_length=50)
-    createtime = models.DateField(auto_now_add=True)
+    order          = models.OneToOneField("Orders", on_delete=models.CASCADE)
+    user           = models.ForeignKey(Users, on_delete=models.CASCADE)
+    card           = models.CharField(max_length=50)
+    payment_method = models.CharField(max_length=50)
+    created_at     = models.DateField(auto_now_add=True)
+    updated_at     = models.DateField(auto_now=True) 
+    class Meta:
+        db_table = 'payments'
 
 class Orders(models.Model):
+    user       = models.ForeignKey(Users, on_delete=models.CASCADE)
     status     = models.ForeignKey("Status", on_delete=models.CASCADE)
     payment    = models.ForeignKey("Payments", on_delete=models.CASCADE)
     address    = models.ForeignKey("Address", on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'orders'
 
 class Status(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        db_table = 'status'
+
 class Carts(models.Model):
-    user         = models.ForeignKey(Users, on_delete=models.CASCADE)
-    product      = models.ForeignKey(Products, on_delete=models.CASCADE)
     order        = models.ForeignKey("Payments", on_delete=models.CASCADE)
+    product      = models.ForeignKey(Products, on_delete=models.CASCADE)
     count        = models.IntegerField(default=1)
     giftwrapping = models.BooleanField()
 
+    class Meta:
+        db_table = 'carts'
+
 class GiftWrapping(models.Model):
     cart           = models.ForeignKey("Carts", on_delete=models.CASCADE,related_name="cart")
-    edition_choies = (
-		('Gold', 'Gold'),
-        ('Silver', 'Silver')
-    )
-    edition     = models.CharField(max_length=6, choices=edition_choies)
+    edition_gold   = 'Gold'
+    edition_silver = 'Silver'
+
+    edition_choies = [
+		(edition_gold, 'Gold'),
+        (edition_silver, 'Silver')
+    ]
+    edition     = models.CharField(max_length=6, choices=edition_choies, default=edition_gold)
     description = models.TextField()
+
+    class Meta:
+        db_table = 'giftwrapping'
 
 class Address(models.Model):
     name    = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     email   = models.EmailField(max_length=254)
 
-
-
+    class Meta:
+        db_table = 'address'
--- product/models.py
+++ product/models.py
@@ -4,11 +4,15 @@ from django.db import models
 class Categories(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        db_table = 'categories'
 
 class SubCategories(models.Model):
     category = models.ForeignKey("Categories", on_delete=models.CASCADE)
     name     = models.CharField(max_length=50)
 
+    class Meta:
+        db_table = 'subcategories'
 
 class Products(models.Model):
     subcategory = models.ForeignKey("SubCategories", on_delete=models.CASCADE)
@@ -17,52 +21,77 @@ class Products(models.Model):
     price        = models.DecimalField(max_digits=18, decimal_places=2) 
     description  = models.TextField()
 
+    class Meta:
+        db_table = 'products'
+
 class Mains(models.Model):
     product = models.ForeignKey("Products", on_delete=models.CASCADE)
     title   = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     mainimage_url = models.CharField(max_length=100)
 
+    class Meta:
+        db_table = 'mains'
     
 
 class Services(models.Model):
     name = models.CharField(max_length=50)
     
+    class Meta:
+        db_table = 'services'
+
 class ProductServices(models.Model):
     product = models.ForeignKey("Products", on_delete=models.CASCADE)
     service = models.ForeignKey("Services", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'productservices'
+
 
 
 class ProductColors(models.Model):
     product = models.ForeignKey("Products", on_delete=models.CASCADE)
     color   = models.ForeignKey("Colors", on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = 'productcolors'
 
 class Colors(models.Model):
     name               = models.CharField(max_length=50)
     colorpickimage_url = models.CharField(max_length=200)
 
+    class Meta:
+        db_table = 'colors'
 
 class ColorImages(models.Model):
     colorimage_url = models.CharField(max_length=200)
     productcolor   = models.ForeignKey("ProductColors", on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = 'colorimages'
 
 class Products_Spec(models.Model):
     product = models.ForeignKey("Products", on_delete=models.CASCADE)
     spec    = models.ForeignKey("Specifications", on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = 'products_spec'
 
 class Specifications(models.Model):
     name = models.CharField(max_length=50)
     data = models.JSONField()
 
+    class Meta:
+        db_table = 'specifications'
 
 class Inspirations(models.Model):
     title            = models.CharField(max_length=50)
     description      = models.TextField()
     video_url        = models.CharField(max_length=200)
     slideimage_url   = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'inspirations'
 
 
 class Features(models.Model):
@@ -72,8 +101,5 @@ class Features(models.Model):
     title        = models.CharField(max_length=50)
     subtitle     = models.CharField(max_length=50)
 
-
-
-
-
-
+    class Meta:
+        db_table = 'features'
